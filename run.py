15dfc238d4b412dcb6c38d0785979707ea6a3c85
ArdaBaranSuludere <ardasuludere361@gmail.com>
2024-01-12 23:22:43 +0300

    .

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from app import app, db
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from flask_sqlalchemy import SQLAlchemy
-
+#yorum satırı
 
 with app.app_context():
     db.create_all()
